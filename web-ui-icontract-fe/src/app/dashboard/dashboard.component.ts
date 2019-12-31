f1f794b326a98fe70c5e5519b66f58b07b44175a
PRIVATE <randallerasmus1@gmail.com>
2019-12-30 19:21:52 -0800

    Adding Http Table

--- web-ui-icontract-fe/src/app/dashboard/dashboard.component.ts
+++ web-ui-icontract-fe/src/app/dashboard/dashboard.component.ts
@@ -1,28 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {catchError, map, startWith, switchMap} from "rxjs/operators";
+import {merge, Observable} from "rxjs";
+import {MatPaginator, MatSort} from "@angular/material";
+
+export interface GithubApi {
+  items: GithubIssue[];
+  total_count: number;
+}
+
+export interface GithubIssue {
+  created_at: string;
+  number: string;
+  state: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements AfterViewInit {
 
-  constructor(private http: HttpClient) { }
+  displayedColumns: string[] = ['created', 'state', 'number', 'title'];
+  exampleDatabase: ExampleHttpDatabase | null;
+  data: GithubIssue[] = [];
 
-  ngOnInit() {
+  resultsLength = 0;
+  isLoadingResults = true;
+  isRateLimitReached = false;
+
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+
+  constructor(private _httpClient: HttpClient) {}
+
+  ngAfterViewInit() {
+    this.exampleDatabase = new ExampleHttpDatabase(this._httpClient);
+
+    // If the user changes the sort order, reset back to the first page.
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+
+    merge(this.sort.sortChange, this.paginator.page)
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          this.isLoadingResults = true;
+          return this.exampleDatabase!.getRepoIssues(
+            this.sort.active, this.sort.direction, this.paginator.pageIndex);
+        }),
+        map(data => {
+          // Flip flag to show that loading has finished.
+          this.isLoadingResults = false;
+          this.isRateLimitReached = false;
+          this.resultsLength = data.total_count;
+
+          return data.items;
+        }),
+        catchError(() => {
+          this.isLoadingResults = false;
+          // Catch if the GitHub API has reached its rate limit. Return empty data.
+          this.isRateLimitReached = true;
+          return observableOf([]);
+        })
+      ).subscribe(data => this.data = data);
   }
+}
+export class ExampleHttpDatabase {
+  constructor(private _httpClient: HttpClient) {}
 
-  upload(files: File[]) {
-    this.basicUpload(files);
-  }
+  getRepoIssues(sort: string, order: string, page: number): Observable<GithubApi> {
+    const href = 'https://api.github.com/search/issues';
+    const requestUrl =
+      `${href}?q=repo:angular/components&sort=${sort}&order=${order}&page=${page + 1}`;
 
-  basicUpload(files: File[]){
-    var formData = new FormData();
-    Array.from(files).forEach(f => formData.append('file', f))
-    this.http.post('https://file.io', formData)
-      .subscribe(event => {
-        console.log('done')
-      })
+    return this._httpClient.get<GithubApi>(requestUrl);
   }
 }
